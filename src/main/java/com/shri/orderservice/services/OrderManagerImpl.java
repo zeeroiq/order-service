712c74676742492fcbcc2a4bd68864e8a423bab1
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-24 00:15:52 +0530

    supporting method to send events. close #29

--- src/main/java/com/shri/orderservice/services/OrderManagerImpl.java
+++ src/main/java/com/shri/orderservice/services/OrderManagerImpl.java
@@ -9,7 +9,11 @@ import com.shri.orderservice.domain.enums.OrderEventEnum;
 import com.shri.orderservice.domain.enums.OrderStatusEnum;
 import com.shri.orderservice.repositories.BeerOrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -24,6 +28,27 @@ public class OrderManagerImpl implements OrderManager {
         beerOrder.setId(null);
         beerOrder.setOrderStatus(OrderStatusEnum.NEW);
         BeerOrder savedBeer = orderRepository.save(beerOrder);
+
+        sendBeerOrderEvent(savedBeer, OrderEventEnum.VALIDATE_ORDER);
         return savedBeer;
+    }
+
+    private void sendBeerOrderEvent(BeerOrder savedBeer, OrderEventEnum enumValidateOrder) {
+
+        StateMachine<OrderStatusEnum, OrderEventEnum> stateMachine = build(savedBeer);
+        Message<OrderEventEnum> msg = MessageBuilder.withPayload(enumValidateOrder).build();
+        stateMachine.sendEvent(msg);
+    }
+
+    private StateMachine<OrderStatusEnum, OrderEventEnum> build(BeerOrder savedBeer) {
+
+        StateMachine<OrderStatusEnum, OrderEventEnum> stateMachine = stateMachineFactory.getStateMachine(savedBeer.getId());
+        stateMachine.stop();
+        stateMachine.getStateMachineAccessor()
+                .doWithAllRegions(sma -> {
+                    sma.resetStateMachine(new DefaultStateMachineContext<>(savedBeer.getOrderStatus(), null, null, null));
+                });
+        stateMachine.start();
+        return stateMachine;
     }
 }
