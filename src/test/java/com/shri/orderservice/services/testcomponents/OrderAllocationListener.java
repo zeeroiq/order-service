f98b25ca76f2fa42917278d41da54b0f5cf5fba3
zeero1q <shrikantvishwakrma74@gmail.com>
2020-10-16 23:35:40 +0530

    Added JMS Listener to handle allocation request

---
+++ src/test/java/com/shri/orderservice/services/testcomponents/OrderAllocationListener.java
@@ -0,0 +1,38 @@
+/*
+ * Created by zeeroiq on 10/15/20, 12:14 PM
+ */
+
+package com.shri.orderservice.services.testcomponents;
+
+import com.shri.model.events.AllocateOrderRequest;
+import com.shri.model.events.AllocateOrderResult;
+import com.shri.orderservice.config.JmsConfig;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderAllocationListener {
+
+    private final JmsTemplate jmsTemplate;
+
+    public OrderAllocationListener(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(Message msg) {
+        AllocateOrderRequest request = (AllocateOrderRequest) msg.getPayload();
+        request.getBeerOrderDto().getBeerOrderLines().forEach(beerOrderLineDto -> {
+            beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+        });
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
+                AllocateOrderResult.builder()
+                .beerOrderDto(request.getBeerOrderDto())
+                .pendingInventory(false)
+                .allocationError(false)
+                .build());
+    }
+}
